7e8fb35e8cf8ee3da42dfdaf23da1a2da2b13980
exosuits <31638842+exosuits@users.noreply.github.com>
2017-09-06 19:30:14 +0900

    Add files via upload

---
+++ Ass1.java
@@ -0,0 +1,10 @@
+package js;
+
+public class Ass1 {
+	public static void main(String[] args){
+		System.out.println("학번 : 2016301060");
+		System.out.println("이름 : 이준성");
+		System.out.println("휴대폰 : 01034335437");
+		System.out.println("GitHub ID: exosuits");
+	}
+}
